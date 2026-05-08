4a15d624bb42c03b7993d1a367591b48f480a913
Christo Sadatua Manatap Pasaribu <christopasaribu90@gmail.com>
2026-05-08 11:03:30 +0700

    Initial commit

---
+++ apps/service-profile/src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors();
+
+  app.useStaticAssets(join(process.cwd(), 'uploads'), {
+  prefix: '/uploads',
+});
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`🚀 Server running on http://localhost:${port}/api`);
+  console.log(`📁 Static files  : http://localhost:${port}/uploads`);
+}
+bootstrap();
