deeafcace513a8060e6224beba986589e355b42e
Christo Sadatua Manatap Pasaribu <christopasaribu90@gmail.com>
2026-05-10 23:33:11 +0700

    fix database render config

--- apps/service-profile/src/config/database.ts
+++ apps/service-profile/src/config/database.ts
@@ -1,11 +1,15 @@
 import { createDatabaseConnection } from '../../../../libs/common/src/utils/dbFactory';
-import dotenv from 'dotenv';
-dotenv.config();
+
+// DEBUG LOG
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
 
 export const dbProfile = createDatabaseConnection(
-    process.env.DB_NAME || 'db_profile',
-    process.env.DB_USER || 'postgres',
-    process.env.DB_PASSWORD || 'postgres_pass',
-    process.env.DB_HOST || 'localhost',
-    parseInt(process.env.DB_PORT || '5434')
+    process.env.DB_NAME!,
+    process.env.DB_USER!,
+    process.env.DB_PASSWORD!,
+    process.env.DB_HOST!,
+    Number(process.env.DB_PORT)
 );
